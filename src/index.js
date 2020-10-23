1ec29b6cfb26ddbbad99b666228cf5abc76e1ec0
Laranto-spb <laranto1985@gmail.com>
2020-10-23 12:41:56 +0300

    feat: Add morse solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,57 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let words = expr.split(' *');
+    let validateInput = expr.split('**********');
+    let newArr = [];
+    let cleanArr = [];
+    let morseArr = [];
+    let decodedArr = [];
+  
+    words.forEach(item => {
+      for (let i = 0; i < item.length; i+=10){
+        newArr.push(item.slice(i, i+10));
+      }
+    });
+  
+    newArr.forEach(item => {
+      if (!item.includes('*')) {
+        cleanArr.push(parseInt(item).toString())
+      } else if (item === '**********') {
+        cleanArr.push(item);
+      }
+    });
+  
+    cleanArr.forEach(item => {
+      let preArr = [];
+      
+          if (item != '**********') {
+        for (i = 0; i<item.length; i+=2) {
+          let sepNumbers = item.slice(i, i+2);
+            if (sepNumbers === '10') {
+              preArr.push ('.'); 
+            } else if (sepNumbers === '11') {
+              preArr.push('-');
+            }
+        } 
+      } else if (item === '**********') {
+        preArr.push(' ');
+      }
+  
+      let symbolArr = preArr.join('');
+      morseArr.push(symbolArr);
+    });
+  
+    morseArr.forEach(item => {
+      decodedArr.push(MORSE_TABLE[item]);
+      if (item === ' ') {
+        decodedArr.push(' ')
+      }       
+    })
+  
+    let final = decodedArr.join('');
+    return final;
+  
 }
 
 module.exports = {
